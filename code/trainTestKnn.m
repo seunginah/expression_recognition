dc099de082b9690b52f61b0553fc923d116432dd
mm40108 <mm40108@massmutual.com>
2016-12-20 20:30:41 -0500

    stash changes

--- code/trainTestKnn.m
+++ code/trainTestKnn.m
@@ -34,5 +34,34 @@ end
 
 %% for for cohn-kanade images
 function ck(x_train, x_test, y_train, y_test)
+
+ model = fitcknn(x_train,y_train,'NumNeighbors',3,...
+        'Standardize',1,'Distance','cosine');
+    [label,score,cost] = predict(model,x_train);
+    
+    correct = 0;
+    n = size(label, 1);
+    for i = 1:n
+        if all(label(i, :) == y_train(i,:))
+            correct = correct + 1;
+        end
+    end
+
+    fprintf('Accuracy of knn on train set:%.2f\n', (correct/n));
+    % disp(label);
+    % disp(y_test);
+
+    [label,score,cost] = predict(model,x_test);
+    correct = 0;
+    n = size(label, 1);
+    for i = 1:n
+        if all(label(i, :) == y_test(i,:))
+            correct = correct + 1;
+        end
+    end
+    fprintf('Accuracy of knn on test set:%.2f\n',mean(label==y_test));
+    %disp(score);
+    % disp(label);
+    % disp(y_test);
 end
 
