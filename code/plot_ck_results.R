17eeea9a1e3bed934a1315893b1ca49f48906491
mm40108 <mm40108@massmutual.com>
2016-12-22 16:18:56 -0500

    final paper edit

--- code/plot_ck_results.R
+++ code/plot_ck_results.R
@@ -10,13 +10,14 @@ results <- read.csv('~/academic/cs670_compvision/expression_recognition/code/res
 
 results <- results %>% gather(model, accuracy, DT:SVM)
 
-f1 <- c('raw pixel', 'pixel_norm1','hog', 'hog_norm1', 'hog_norm2', 'lbp', 'gabor', 'gabor_norm1', 'pixel_sharpen', 'pixel_sharpen_norm1')
-f2 <- c('fiducial_points', 'fiducial_points_n1','soft_clustering','soft_clustering_norm2', 'pixel_gradient')
+f1 <- c('pixel_norm1','hog', 'hog_norm1', 'hog_norm2', 'lbp', 'gabor', 'gabor_norm1', 'pixel_sharpen', 'pixel_sharpen_norm1')
+f2 <- c('fiducial_points', 'fiducial_points_n1','soft_clustering', 'soft_clustering_norm1', 'soft_clustering_norm2', 'raw pixel', 'pixel_gradient')
 
-df1 <- results %>% filter(feature %in% f1) %>% arrange(feature)
-df1 <- df1[c(19:21, 7:18, 1:6, 22:27), ]
-df2 <- results %>% filter(feature %in% f2) %>% arrange(feature)
-df2 <- df2[c(1:6, 10:15, 7:9), ]
+df1 <- results %>% filter(feature %in% f1)
+df2 <- results %>% filter(feature %in% f2)
+
+df1$feature <- factor(df1$feature, levels = f1)
+df2$feature <- factor(df2$feature, levels = f2)
 
 feats1 <- ggplot(df1, aes(x = feature, y = accuracy, fill = model)) +
   geom_bar(stat = "identity", position = "dodge") +
